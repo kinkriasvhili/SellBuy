25d1a4a98fb85563e6d41e402d763ca417825a29
kinkriashvili <zhiend0331@gmail.com>
2025-10-11 18:22:14 +0400

    styles

--- sell-buy-front/src/Components/navigation/Search.jsx
+++ sell-buy-front/src/Components/navigation/Search.jsx
@@ -3,10 +3,12 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { div } from "framer-motion/m";
 
 export default function Search() {
   const [inputFocus, setInputFocus] = useState(false);
   const [value, setValue] = useState("");
+  const [searchOn, setSearchOn] = useState(false);
   const navigate = useNavigate();
 
   const handleSearch = () => {
@@ -15,6 +17,7 @@ export default function Search() {
     } else {
       navigate(`/products`);
     }
+    setSearchOn(false);
   };
 
   const handleKeyDown = (e) => {
@@ -24,23 +27,56 @@ export default function Search() {
   };
 
   return (
-    <div
-      className={`${styles.searchBox} flex-center ${
-        inputFocus ? styles.focused : ""
-      }`}
-    >
-      <input
-        type="text"
-        placeholder="Search products..."
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
-        onFocus={() => setInputFocus(true)}
-        onBlur={() => setInputFocus(false)}
-        onKeyDown={handleKeyDown} // allow Enter to trigger search
-      />
-      <button onClick={handleSearch}>
+    <>
+      <div
+        className={`${styles.searchBox} flex-center ${
+          inputFocus ? styles.focused : ""
+        }`}
+      >
+        <input
+          type="text"
+          placeholder="Search products..."
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onFocus={() => setInputFocus(true)}
+          onBlur={() => setInputFocus(false)}
+          onKeyDown={handleKeyDown} // allow Enter to trigger search
+        />
+        <button onClick={handleSearch}>
+          <FontAwesomeIcon icon={faMagnifyingGlass} size="1x" />
+        </button>
+      </div>
+      <button
+        className={styles.searchButResp}
+        onClick={() => {
+          setSearchOn((prev) => !prev);
+        }}
+      >
         <FontAwesomeIcon icon={faMagnifyingGlass} size="1x" />
       </button>
-    </div>
+      {searchOn && (
+        <div className={styles.modalOverlay} onClick={() => setSearchOn(false)}>
+          <div
+            className={`${styles.searchBoxResponsive} flex-center ${
+              inputFocus ? styles.focusedResponsive : ""
+            }`}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <input
+              type="text"
+              placeholder="Search products..."
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              onFocus={() => setInputFocus(true)}
+              onBlur={() => setInputFocus(false)}
+              onKeyDown={handleKeyDown} // allow Enter to trigger search
+            />
+            <button onClick={handleSearch}>
+              <FontAwesomeIcon icon={faMagnifyingGlass} size="1x" />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
